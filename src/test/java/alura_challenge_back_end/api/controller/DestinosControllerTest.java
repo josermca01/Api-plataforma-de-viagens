4f9cfa2b012b07e94708a68e12b21dda1768240c
josermca01 <josermca01@gmail.com>
2024-08-27 21:10:22 -0300

    Finalizacao-dos-casos-de-teste-do-crud

--- src/test/java/alura_challenge_back_end/api/controller/DestinosControllerTest.java
+++ src/test/java/alura_challenge_back_end/api/controller/DestinosControllerTest.java
@@ -14,8 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import alura_challenge_back_end.api.entities.Destinos;
 import alura_challenge_back_end.api.repository.DestinosRepository;
@@ -55,7 +54,7 @@ public class DestinosControllerTest {
     private Long userId = 1L;
 
     @Test
-    @DisplayName("Validar codigo 200")
+    @DisplayName("Validar codigo 200 da maneira que os dados sao enviados quando solicitados pela requisisao get")
     void consultaCenarioGet1() throws Exception{
         List<Destinos> lista = insertDestinos();
         when(service.getDestino(anyString())).thenReturn(lista);
@@ -71,7 +70,7 @@ public class DestinosControllerTest {
         assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
     }
     @Test
-    @DisplayName("Validar codigo 200")
+    @DisplayName("Validar codigo 200 quando todos os campos sao preechidos")
     void cadastroCenarioPost1() throws Exception{
         MockMultipartFile file = new MockMultipartFile(
                         "image",
@@ -93,7 +92,7 @@ public class DestinosControllerTest {
         assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
     }
     @Test
-    @DisplayName("Validar codigo 400")
+    @DisplayName("Validar codigo 400 quando nenhum campo e enviado")
     void cadastroCenarioPost2() throws Exception{
         var response = mvc
                 .perform(MockMvcRequestBuilders.multipart("/destinos"))
@@ -103,7 +102,7 @@ public class DestinosControllerTest {
                 .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
     @Test
-    @DisplayName("Validar codigo 400")
+    @DisplayName("Validar codigo 400 quando o campo do arquivo de imagem nao e enviado")
     void cadastroCenarioPost3() throws Exception{
 
         var response = mvc
@@ -115,7 +114,7 @@ public class DestinosControllerTest {
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
     @Test
-    @DisplayName("Validar codigo 400")
+    @DisplayName("Validar codigo 400 quando campo do nome e do preco nao sao enviados")
     void cadastroCenarioPost4() throws Exception{
 
         MockMultipartFile file = new MockMultipartFile(
@@ -129,7 +128,59 @@ public class DestinosControllerTest {
 
         assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
+    @Test
+    @DisplayName("Teste de requisicoes")
+    void deletarCenarioDelete1() throws Exception {
 
+        doNothing().when(service).delete(userId);
+
+        mvc.perform(delete("/destinos/"+userId.toString())
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNoContent());
+    }
+    @Test
+    @DisplayName("Teste de requisicoes")
+    void deletarCenarioDelete2() throws Exception {
+        var dados = new Destinos(87L,"aracoiaba","teste.png",null,2000.00);
+
+
+        Mockito.doThrow(new ResponseStatusException(HttpStatus.NOT_FOUND, "Place not found")).when(service).delete(dados.getId());
+
+
+        mvc.perform(delete("/destinos/" + dados.getId().toString())
+        .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isNotFound());
+
+    }
+
+    @Test
+    @DisplayName("Deveria devolver codigo http 400 quando informacoes estao invalidas")
+    void alterar_cenario1_put() throws Exception {
+        var response = mvc
+                .perform(put("/depoimentos/{id}",userId))
+                .andReturn().getResponse();
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+    @Test
+    @DisplayName("Deveria devolver codigo http 200 quando informacoes estivrem validas")
+    void alterar_cenario2_put() throws Exception {
+        var dados = new Destinos(userId,"novo nome","test.png",null,900.00);
+        
+        when(service.update(eq(dados.getId()),any())).thenReturn(dados);
+
+        var json = dadosDestinos.write(dados).getJson();
+
+        var response = mvc
+                .perform(put("/destinos/{id}",userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json))
+                .andReturn().getResponse();
+        
+        assertThat(response.getContentAsString()).isEqualTo(json);
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+    }
     private List<Destinos> insertDestinos(){
         List<Destinos> allDestinos = new ArrayList<Destinos>();
         List<String> fotos = insertFotos();
